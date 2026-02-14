a8e542c3d660f1fccd139e58746c46ef4d8eeaa7
ibrahimrizk56 <idlmmaf56@gmail.com>
2026-02-14 20:40:31 +0200

    PCC-Chapter 2 Exercise

---
+++ Chapter 2/11-Adding Comments.py
@@ -0,0 +1,13 @@
+# 2-11. Adding Comments: Choose two of the programs you’ve written, and
+# add at least one comment to each. If you don’t have anything specific to
+# write because your programs are too simple at this point, just add your
+# name and the current date at the top of each program file. Then write one
+# sentence describing what the program does.
+
+#print "Hello World!"
+print("Hello World!")
+#Assign values to variables
+x , y, z = 10 , 20 , 30
+#print point values in three lines
+
+print(f"Point P1 values are :\nx = {x}\ny = {y}\nz = {z}")
